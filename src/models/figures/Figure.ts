9befa82f5e045617343a4235fcd49b92a347f47e
AntonSheluho <grinfrend2@gmail.com>
2022-05-18 16:25:30 +0300

    add board and figures

---
+++ src/models/figures/Figure.ts
@@ -0,0 +1,38 @@
+import { Cell } from './../Cell';
+import { Colors } from './../Colors';
+import logo from '../../assets/black-king.png'
+
+export enum FigureName {
+    FIGURE = 'Figure',
+    KING = 'King',
+    KNIGHT = 'Knight',
+    PAWN = 'Pawn',
+    QUEEN = 'Queen',
+    ROOK = 'Rook',
+    BISHOP = 'Bishop',
+}
+
+export class Figure {
+    color: Colors;
+    logo: typeof logo | null;
+    cell: Cell;
+    name: FigureName;
+    id: number;
+
+    constructor(color: Colors, cell: Cell) {
+        this.cell = cell;
+        this.color = color;
+        this.cell.figure = this;
+        this.logo = null;
+        this.name = FigureName.FIGURE;
+        this.id = Math.random()
+    }
+
+    canMove(target: Cell): boolean {
+        return true;
+    }
+    moveFigure(target: Cell) {
+
+    }
+
+}
